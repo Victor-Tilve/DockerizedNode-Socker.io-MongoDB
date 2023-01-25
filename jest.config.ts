22716cd8c3993916bfec4fdfb4c9b3eecee062bf
Victor-Tilve <ing.victortilve@gmail.com>
2023-01-24 23:22:38 -0600

    chore: starting point, vscode,eslint,lintstage,jest config, package, readme, tsconfig

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  roots: ['./src'],
+  collectCoverageFrom: ['./src/**/*.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  modulePaths: [
+    '<rootDir>'
+  ]
+}
---
+++ jest.config.unit.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  roots: ['<rootDir>/src', '<rootDir>'],
+  collectCoverageFrom: ['<rootDir>/src/***/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  modulePathIgnorePatterns: ['./src/interfaces'],
+  moduleDirectories: ['node_modules'],
+  testMatch: ['**/*spec.ts'],
+  modulePaths: [
+    '<rootDir>'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/mock/redis/jest.setup.redis-mock.ts']
+}
+
+export default config
